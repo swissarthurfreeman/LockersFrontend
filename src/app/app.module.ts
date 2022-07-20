5f0267b761698d428ac6b18f418390b4666d5c52
A. Freeman <arthur.freemna@etu.unige.ch>
2022-07-20 20:35:41 +0200

    ait, no / in routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SitesComponent } from './sites/sites.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SitesComponent
   ],
   imports: [
     BrowserModule,
